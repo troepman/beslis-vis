0015b9fc7f60ddaa1e5e47c5ee5c6fc898be7535
Thore Roepman <thore.roepman@gmail.com>
2022-10-28 11:45:22 +0200

    Moved to react

--- main.js
+++ main.js
@@ -1,52 +1,19 @@
-function setupBubblesAnimation(){
-    const bubbles = Array.from(document.getElementsByClassName("bubble"));
-    function generateState(i, init=false) {
-        const paddingWidth = 50;
-        const contentWidth = window.innerWidth - 2 * paddingWidth
-        return [
-            paddingWidth + (i + Math.random()) * (contentWidth / bubbles.length), 
-            (init?Math.random():1) * window.innerHeight, 
-            Math.random() * 50 + 50
-        ];
-    }
-    const states = bubbles.map((_, i) =>  generateState(i, true))
-    function animate() {
-        bubbles.forEach((bubble, i) => {
-            const state = states[i]
-            state[1] -= 1;
-            if (state[1] < -100) {
-                const n = generateState(i);
-                state[0] = n[0]
-                state[1] = n[1];
-            }
-            bubble.style.top = state[1] + "px";
-            bubble.style.left = (state[0] - state[2] / 2) + "px";
-            bubble.style.width = bubble.style.height = state[2] + "px";
-        });
-
-        window.requestAnimationFrame(animate);
-    }
-    window.requestAnimationFrame(animate);
-}
-
-
-
 let applicationState = {
-    state: "Question",
+    state: 'Question',
     question: null,
     videoFragment: null,
     videoReady: false,
     videoEnded: false,
-}
+};
 
-const screens = {}
+const screens = {};
 
 const NUMBER_OF_FRAGMENTS = 85;
 
-const randomQuestions = ['Heb ik zin in koffie?', 'Trek ik een korte broek aan?', 'Ga ik uit vanavond?']
+const randomQuestions = ['Heb ik zin in koffie?', 'Trek ik een korte broek aan?', 'Ga ik uit vanavond?'];
 
-function showScreen(name){
-    for (const screenName in screens){
+function showScreen(name) {
+    for (const screenName in screens) {
         const screen = screens[screenName];
         screen.style.display = 'none';
     }
@@ -58,36 +25,35 @@ function onEndOfSearchAnimation() {
         setTimeout(onEndOfSearchAnimation, 500);
         return;
     }
-    newState({state:'Answer'})
+    newState({ state: 'Answer' });
 }
 
 function newState(modification) {
-    const newState = {...applicationState, ...modification}
+    const newState = { ...applicationState, ...modification };
     switch (modification.state) {
-        case "Question":
-            showScreen("question");
-        break;
-        case "Feeding":
-            showScreen("feeding");
+        case 'Question':
+            showScreen('question');
+            break;
+        case 'Feeding':
+            showScreen('feeding');
             const videoElement = document.getElementById('video-answer');
-            videoElement.src = newState.videoFragment
+            videoElement.src = newState.videoFragment;
             videoElement.load();
             newState.videoReady = false;
-            
 
             setTimeout(() => {
                 onEndOfSearchAnimation();
             }, 6000);
-            startSearchAnimation()
+            startSearchAnimation();
             document.getElementById('feeding-question').innerHTML = newState.question;
-        break;
-        case "Answer":
+            break;
+        case 'Answer':
             stopSearchAnimation();
-            showScreen("answer");
+            showScreen('answer');
             document.getElementById('video-answer').play();
             setTimeout(() => {
-                if (applicationState.state === 'Answer') { 
-                    onVideoEnded(); 
+                if (applicationState.state === 'Answer') {
+                    onVideoEnded();
                 }
             }, 15100);
             document.getElementById('video-answer-question').innerText = newState.question;
@@ -102,69 +68,67 @@ function newState(modification) {
             setTimeout(() => {
                 document.getElementById('video-overlay-divider').style.opacity = 1;
                 document.getElementById('video-answer-status').style.opacity = 0;
-            }, 8000)
-        break;
-        case "Done":
+            }, 8000);
+            break;
+        case 'Done':
             document.getElementById('video-overlay-yes').style.display = '';
             document.getElementById('video-overlay-no').style.display = '';
             document.getElementById('answer-navigate').style.display = '';
             document.getElementById('video-answer-status').style.display = 'None';
-        break;
-
+            break;
     }
-    applicationState = newState
+    applicationState = newState;
 }
 
-function formatQuestion(q){
+function formatQuestion(q) {
     q = q.trim();
-    if (!q.endsWith('?')){
-        q += '?'
+    if (!q.endsWith('?')) {
+        q += '?';
     }
     return q;
 }
 
-function onAskQuestion(ev, random=false) {
+function onAskQuestion(ev, random = false) {
     const question = formatQuestion(
-        random 
-        ? randomQuestions[Math.round(Math.random() * (randomQuestions.length - 1))] 
-        : document.getElementById('question-field').value
+        random
+            ? randomQuestions[Math.round(Math.random() * (randomQuestions.length - 1))]
+            : document.getElementById('question-field').value,
     );
-    
+
     index = Math.floor(Math.random() * NUMBER_OF_FRAGMENTS);
-    newState({state:"Feeding", question, videoFragment: `fragments/fragment${index}.mp4`});
+    newState({ state: 'Feeding', question, videoFragment: `fragments/fragment${index}.mp4` });
 
     ev.preventDefault();
 }
 
 function onVideoReady() {
-    if (applicationState.state !== 'Feeding') { 
+    if (applicationState.state !== 'Feeding') {
         return;
     }
 
-    newState({videoReady: true})
+    newState({ videoReady: true });
 }
 
 function onVideoEnded() {
-    newState({state:'Done'})
+    newState({ state: 'Done' });
 }
 
-function onGoHome(){
-    newState({state:'Question'})
+function onGoHome() {
+    newState({ state: 'Question' });
 }
 
 function onAskSameQuestion() {
     index = Math.floor(Math.random() * NUMBER_OF_FRAGMENTS);
-    newState({state:"Feeding", videoFragment: `fragments/fragment${index}.mp4`});
+    newState({ state: 'Feeding', videoFragment: `fragments/fragment${index}.mp4` });
 }
 
-function parseUrl(){
+function parseUrl() {
     const query = window.location.search.substring(1);
     const vars = query.split('&');
-    const result = {}
+    const result = {};
     for (let i = 0; i < vars.length; i++) {
         const pair = vars[i].split('=');
         result[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
-        
     }
 
     return result;
@@ -187,59 +151,66 @@ const searchAnimation = {
         searchSpans['s1'].style.opacity = 0;
         searchSpans['s2'].style.opacity = 0;
         searchSpans['s3'].style.opacity = 1;
-    }
-}
+    },
+};
 let searchAnimationStart = 0;
 let searchAnimationHandle;
 function searchAnimationRunner() {
     const duration = (Date.now() - searchAnimationStart) / 1000;
-    const key = Object.keys(searchAnimation).reduce((best, timestamp) => timestamp <= duration ? timestamp : best, undefined)
-    if (key){
+    const key = Object.keys(searchAnimation).reduce(
+        (best, timestamp) => (timestamp <= duration ? timestamp : best),
+        undefined,
+    );
+    if (key) {
         searchAnimation[key]();
     }
 
-    if (duration > 6){
+    if (duration > 6) {
         searchAnimationStart = Date.now();
     }
-    searchAnimationHandle = requestAnimationFrame(searchAnimationRunner)
+    searchAnimationHandle = requestAnimationFrame(searchAnimationRunner);
 }
 
-function startSearchAnimation(){
-    searchAnimationStart = Date.now()
+function startSearchAnimation() {
+    searchAnimationStart = Date.now();
     searchAnimationRunner();
 }
 function stopSearchAnimation() {
-    if (searchAnimationHandle){
+    if (searchAnimationHandle) {
         cancelAnimationFrame(searchAnimationHandle);
     }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("go-button").addEventListener('click', onAskQuestion);
-    document.getElementById("go-form").addEventListener('submit', onAskQuestion);
-    document.getElementById("random-button").addEventListener('click', (ev) => onAskQuestion(ev, true))
-    document.getElementById("video-answer").addEventListener('loadeddata', onVideoReady);
+    document.getElementById('go-button').addEventListener('click', onAskQuestion);
+    document.getElementById('go-form').addEventListener('submit', onAskQuestion);
+    document.getElementById('random-button').addEventListener('click', (ev) => onAskQuestion(ev, true));
+    document.getElementById('video-answer').addEventListener('loadeddata', onVideoReady);
     document.getElementById('video-answer').addEventListener('ended', onVideoEnded);
     document.getElementById('back-button').addEventListener('click', onGoHome);
     document.getElementById('again-button').addEventListener('click', onAskSameQuestion);
 
-    for (const screen of document.getElementsByTagName("screen")){
+    for (const screen of document.getElementsByTagName('screen')) {
         screens[screen.id] = screen;
     }
 
-    for (const searchSpan of document.getElementsByClassName("search")){
+    for (const searchSpan of document.getElementsByClassName('search')) {
         searchSpans[searchSpan.id] = searchSpan;
     }
 
     setupBubblesAnimation();
 
-    const query = parseUrl()
-    if ('q' in query){
+    const query = parseUrl();
+    if ('q' in query) {
         const index = Math.round(Math.random() * 118);
-        const fragment = query['a'] ?? `fragment${index}`
-        
-        newState({state:"Feeding", question: formatQuestion(query['q']), videoFragment: `fragments/${fragment}.mp4`});
+        const fragment = query['a'] ?? `fragment${index}`;
+
+        newState({
+            state: 'Feeding',
+            question: formatQuestion(query['q']),
+            videoFragment: `fragments/${fragment}.mp4`,
+        });
     } else {
-        newState({state:"Question"});
+        newState({ state: 'Question' });
     }
-})
+});
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,238 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import Screen from './components/screen';
+import BubbleContainer from './components/bubble-container';
+
+function formatQuestion(q: string) {
+    q = q.trim();
+    if (!q.endsWith('?')) {
+        q += '?';
+    }
+    return q;
+}
+
+function generateRandomQuestion(): string {
+    const randomQuestions = ['Heb ik zin in koffie?', 'Trek ik een korte broek aan?', 'Ga ik uit vanavond?'];
+    return randomQuestions[Math.round(Math.random() * (randomQuestions.length - 1))];
+}
+
+function formatVideoUrl(fragment?: string) {
+    if (fragment) {
+        return `fragments/${fragment}.mp4`;
+    } else {
+        const n = Math.floor(85 * Math.random());
+        return `fragments/fragment${n}.mp4`;
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const videoRef = React.useRef<HTMLVideoElement>(null);
+
+    const [appState, setAppState] = React.useState<{
+        question: string;
+        videoFragment: string;
+        videoReady: boolean;
+        feedingAnimation: number;
+        state: 'Question' | 'Loading' | 'Swimming' | 'Done';
+    }>({
+        question: '',
+        videoFragment: '',
+        videoReady: false,
+        state: 'Question',
+        feedingAnimation: 0,
+    });
+
+    const submitQuestion = React.useCallback(
+        (q?: string) => {
+            const question = formatQuestion(q ?? generateRandomQuestion());
+            setAppState((c) => ({
+                ...c,
+                question,
+                state: 'Loading',
+                feedingAnimation: 0,
+                videoFragment: formatVideoUrl(),
+                videoReady: false,
+            }));
+        },
+        [setAppState],
+    );
+
+    const onVideoReady = React.useCallback(() => {
+        setAppState((c) => ({ ...c, videoReady: true }));
+        console.log('VideoReady');
+    }, [setAppState]);
+
+    const onDifferentFish = React.useCallback(() => {
+        setAppState((c) => ({
+            ...c,
+            state: 'Loading',
+            feedingAnimation: 0,
+            videoFragment: formatVideoUrl(),
+            videoReady: false,
+        }));
+    }, [setAppState]);
+
+    const onNewQuestion = React.useCallback(() => {
+        setAppState((c) => ({
+            ...c,
+            state: 'Question',
+        }));
+    }, [setAppState]);
+
+    useEffect(() => {
+        if (appState.state === 'Loading' && videoRef.current) {
+            videoRef.current.src = appState.videoFragment;
+            videoRef.current.load();
+            const handle = setInterval(
+                () =>
+                    setAppState((c) => ({
+                        ...c,
+                        feedingAnimation: c.feedingAnimation + 1,
+                    })),
+                2000,
+            );
+            return () => clearInterval(handle);
+        }
+        if (appState.state === 'Swimming' && videoRef.current) {
+            videoRef.current.play();
+            setTimeout(() => setAppState((c) => ({ ...c, state: 'Done' })), 15000);
+        }
+    }, [appState.state]);
+
+    useEffect(() => {
+        console.log(appState);
+        if (appState.feedingAnimation > 2) {
+            setAppState((c) => ({ ...c, state: 'Swimming' }));
+        }
+    }, [appState.feedingAnimation, appState.videoReady]);
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.addEventListener('load', () => setAppState((c) => ({ ...c, videoReady: true })));
+        }
+    }, [videoRef.current]);
+
+    return (
+        <div className="App">
+            <header id="header">
+                <a href="/" className="no-style">
+                    <h1>Beslis vis</h1>
+                </a>
+            </header>
+            <div className="content">
+                <Screen visible={['Question'].includes(appState.state)}>
+                    <form
+                        id="go-form"
+                        onSubmit={(ev) => {
+                            submitQuestion(appState.question);
+                            ev.preventDefault();
+                        }}
+                    >
+                        <p className="question-intro">Twijfel je tussen Ja of Nee?</p>
+                        <p className="question-intro">Denk niet verder maar laat onze goudvis voor je kiezen!</p>
+                        <div className="line vspace">
+                            <input
+                                className="text-input"
+                                type="text"
+                                id="question-field"
+                                name="question"
+                                placeholder="Ga ik ... Ja of Nee?"
+                                value={appState.question}
+                                onChange={(ev) => setAppState((c) => ({ ...c, question: ev.target.value }))}
+                            />
+                            <div className="go-button-wrapper">
+                                <div
+                                    className="go button primary"
+                                    id="go-button"
+                                    onClick={() => submitQuestion(appState.question)}
+                                >
+                                    {'>>'}
+                                </div>
+                            </div>
+                        </div>
+                        <p className="question-intro">Of... Stel een random vraag</p>
+                        <div className="line vspace">
+                            <div className="button" id="random-button" onClick={() => submitQuestion()}>
+                                Verras me
+                            </div>
+                        </div>
+                    </form>
+                </Screen>
+                <Screen
+                    visible={['Loading'].includes(appState.state)}
+                    style={{
+                        alignItems: 'center',
+                        flexDirection: 'column',
+                        justifyContent: 'space-around',
+                        display: 'flex',
+                    }}
+                >
+                    <span className="feeding-question" id="feeding-question">
+                        {appState.question}
+                    </span>
+                    <div className="search-wrapper">
+                        <span
+                            className="search opacity-fade05"
+                            style={{ opacity: appState.feedingAnimation % 3 === 0 ? 1 : 0 }}
+                        >
+                            Vissenkom schoonmaken.
+                        </span>
+                        <span
+                            className="search opacity-fade05"
+                            style={{ opacity: appState.feedingAnimation % 3 === 1 ? 1 : 0 }}
+                        >
+                            Vis zoeken..
+                        </span>
+                        <span
+                            className="search opacity-fade05"
+                            style={{ opacity: appState.feedingAnimation % 3 === 2 ? 1 : 0 }}
+                        >
+                            Jouw vraag voeren...
+                        </span>
+                    </div>
+                </Screen>
+                <Screen
+                    visible={['Swimming', 'Done'].includes(appState.state)}
+                    style={{
+                        justifyContent: 'space-around',
+                        alignItems: 'center',
+                        display: 'flex',
+                        flexDirection: 'column',
+                    }}
+                >
+                    <span className="video-answer-question" id="video-answer-question">
+                        {appState.question}
+                    </span>
+                    <div className="video-answer-wrapper">
+                        <video id="video-answer" ref={videoRef} muted onLoadedData={onVideoReady}></video>
+                        <div className="video-answer-side no" id="video-overlay-no">
+                            <span>Nee</span>
+                        </div>
+                        <div className="video-answer-divider" id="video-overlay-divider"></div>
+                        <div className="video-answer-side yes" id="video-overlay-yes">
+                            <span>Ja</span>
+                        </div>
+                    </div>
+                    {appState.state === 'Swimming' && (
+                        <span id="video-answer-status" className="opacity-fade05">
+                            Verteren van de vraag, dit kost tijd...
+                        </span>
+                    )}
+                    {appState.state === 'Done' && (
+                        <div className="line" id="answer-navigate">
+                            <div className="button primary" id="back-button" onClick={onNewQuestion}>
+                                {'<< Stel nieuwe vraag!'}
+                            </div>
+                            <div className="button" id="again-button" onClick={onDifferentFish}>
+                                Andere vis {'>>'}
+                            </div>
+                        </div>
+                    )}
+                </Screen>
+            </div>
+            <BubbleContainer />
+        </div>
+    );
 }
 
 export default App;
